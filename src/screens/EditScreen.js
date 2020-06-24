d533a90ecbab1979dda32b8a21d6db4e2221bc9f
AIGelman23 <AIGelman23@gmail.com>
2020-06-24 19:17:34 -0400

    First commit

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
+
+const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+
+  return <BlogPostForm />;
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
